b9c3eb3e03545e2e22bf4f7dc3e9758f43569c0f
Argann Bonneau <argann.b@hotmail.fr>
2017-07-31 18:15:57 +0200

    V0.0.2 - Functionnal chat

---
+++ Assets/Scripts/Web/ConnectionManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Quobject.SocketIoClientDotNet.Client;
+
+public static class ConnectionManager {
+
+    private static Socket socket;
+
+    public static Socket Socket {
+        get { return socket; }
+        set { socket = value; }
+    }
+
+
+    public static void Connect() {
+        socket = IO.Socket("http://192.168.99.100:5000/");
+
+        socket.On(Socket.EVENT_CONNECT, () => {
+            socket.Emit("hi");
+        });
+    }
+
+    public static void Disconnect() {
+        socket.Disconnect();
+    }
+}
---
+++ Assets/Scripts/Web/Login.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEngine.SceneManagement;
+
+namespace web_assets {
+
+    public class Login : MonoBehaviour {
+
+        [SerializeField]
+        private InputField username;
+
+        [SerializeField]
+        private InputField password;
+
+        private bool isLogged;
+
+	    // Use this for initialization
+	    void Start () {
+            isLogged = false;
+            ConnectionManager.Connect();
+            addListeners();
+        }
+
+        void Update() {
+            if (isLogged) {
+                SceneManager.LoadSceneAsync("Lobby", LoadSceneMode.Single);
+                isLogged = false;
+            }
+        }
+	
+	    public void TryToLogin() {
+            Dictionary<string, string> credentials = new Dictionary<string, string>();
+
+            credentials.Add("username", username.text);
+            credentials.Add("password", password.text);
+
+            ConnectionManager.Socket.Emit("login", JsonConvert.SerializeObject(credentials));
+        }
+
+        void OnApplicationQuit() {
+            ConnectionManager.Disconnect();
+        }
+
+        private void addListeners() {
+            ConnectionManager.Socket.On("login-response", (d) => {
+                JObject data = (JObject)d;
+
+                if ((bool)data["success"]) {
+                    isLogged = true;
+                } else {
+                    Debug.Log("It's not OK");
+                }
+            });
+        }
+    }
+}
